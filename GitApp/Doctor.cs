b6de92ee67e23c6cf78b4b8a05436ebcc5511dd8
hhcodecademy <huseyn.hasanli@code.edu.az>
2024-05-02 22:55:07 +0400

    add constructor for Doctor

--- GitApp/Doctor.cs
+++ GitApp/Doctor.cs
@@ -8,11 +8,20 @@ namespace GitApp
 {
     internal class Doctor
     {
+        public Doctor(int id, decimal salary, int departmentId)
+        {
+            Id = id;
+            Salary = salary;
+            DepartmentId = departmentId;
+        }
+
+
         public int Id { get; set; }
         public decimal Salary { get; set; }
         public int DepartmentId { get; set; }
         public Department Department { get; set; }
         public ICollection<Diagnostic> Diagnostics { get; set; }
+      
 
     }
 }
