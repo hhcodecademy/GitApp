ac3140f4c592d7749e639841d7a891ddb6c93c65
hhcodecademy <huseyn.hasanli@code.edu.az>
2024-05-02 22:31:56 +0400

    change program file

--- GitApp/Program.cs
+++ GitApp/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Department department= new Department();
+            
+            Doctor doctor = new Doctor();
+            doctor.Id = 1;
+            department.Doctors.Add(doctor);
         }
     }
 }
